ce4af4d978a4ab3f9e7a014454810e7411356a10
The Leo <chaihaolin2002@gmail.com>
2020-04-04 14:36:53 -0400

    math IA

---
+++ 2018-2019/Spanish/quizlet hack.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+var set = prompt("wat is the set number");
+var question = $('.FormattedText.notranslate.TermText.StudentPrompt-promptText').getAttribute("aria-label");
+var prevQuestion;
+if(question !== prevQuestion){
+	//question changed, search for ans;
+	var corAns = ;//insert correct ans, search from data base
+	
+}
